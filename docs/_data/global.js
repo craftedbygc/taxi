19ecc7d626ddab10e259197564929256f0f07011
Jake Whiteley <odegramusic@gmail.com>
2022-07-26 18:50:18 +0100

    update path var for docs

--- docs/_data/global.js
+++ docs/_data/global.js
@@ -1,4 +1,4 @@
 module.exports = {
     metaDesc: "Taxi is a \uD83E\uDD0F small and \uD83D\uDC0A snappy js library for adding slick PJAX navigation and beautiful transitions to your website.",
-    url: process.env.ENVIRONMENT ? "/taxi" : "http://localhost:3000"
+    url: process.env.ENVIRONMENT ? "https://taxi.js.org" : "http://localhost:3000"
 }
