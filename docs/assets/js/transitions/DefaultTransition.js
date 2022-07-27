984b8b61b897b2b6fa3de242b268c94d3cb45b6d
Jake Whiteley <odegramusic@gmail.com>
2022-07-27 21:03:30 +0100

    play with tail

--- docs/assets/js/transitions/DefaultTransition.js
+++ docs/assets/js/transitions/DefaultTransition.js
@@ -7,7 +7,7 @@ export default class DefaultTransition extends Transition {
 
         gsap.timeline()
             .to(this.wrapper, { opacity: 0 }, 0)
-            .to(overlay, { width: '100%', ease: 'power2.inOut' }, 0)
+            .to(overlay, { width: '100%', ease: 'power3.in'}, 0)
             .then(() => {
                 done()
             })
@@ -15,12 +15,15 @@ export default class DefaultTransition extends Transition {
 
     onEnter({ to, trigger, done }) {
         const overlay = document.querySelector('.js-overlay')
+        const tail = document.querySelector('.js-overlay-tail')
+        const tailWidth = tail.getBoundingClientRect().width
 
         gsap.timeline()
-            .to(overlay, { x: '100%', ease: 'power2.inOut' }, 0)
+            .to(overlay, { x: '100%', ease: 'power3.out', duration: 1 }, 0)
+            .to(tail, { x: 0, width: 0, ease: 'power3.out', duration: 1.3 }, 0)
             .to(this.wrapper, { opacity: 1 }, 0)
             .then(() => {
-
+                gsap.set(tail, {width: tailWidth, x: -tailWidth})
                 gsap.set(overlay, {width: 0, x: 0})
                 done()
             })
